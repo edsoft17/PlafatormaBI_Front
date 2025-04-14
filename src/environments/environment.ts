3aee5a4ba14a044792cf963fc2e9ea1c2b67ee54
edward <edward_jara@outlook.es>
2025-04-14 02:40:23 -0500

    integracion con somee

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,3 +1,3 @@
 export const environment = {
-    baseUrlBI: "https://aplicativos.linea.pe/api_lineaBI"//"https://soluciones.linea.pe/aplicativos/api_lineaBI",
+    baseUrlBI: "http://lineaback.somee.com"//https://aplicativos.linea.pe/api_lineaBI//"https://soluciones.linea.pe/aplicativos/api_lineaBI",
 };
